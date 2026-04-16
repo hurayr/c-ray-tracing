60b66d7644fb73941cb85d2570b532216808cc2e
HurayrahB <huray2007@gmail.com>
2026-04-16 00:42:42 -0400

    Initial Commit
    Beginning of project. Experimented with CMake ppm files, created classes for vectors and colours

---
+++ build/CMakeFiles/myGradient.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myGradient.dir/src/main.cpp.obj"
+  "CMakeFiles/myGradient.dir/src/main.cpp.obj.d"
+  "libmyGradient.dll.a"
+  "myGradient.exe"
+  "myGradient.exe.manifest"
+  "myGradient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myGradient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "vec3.h"
+#include "colour.h"
+
+#include <iostream>
+
+int main();
+
+int main() {
+    // image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j {0}; j < image_height; j++) {
+        // progress indicator
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        // image generation
+        for (int i{0}; i < image_width; i++) {
+           auto pixel_colour = colour( double(i)/(image_width-1), double(j)/(image_height-1), 0 );
+           write_colour(std::cout, pixel_colour);
+        };
+    };
+
+    std::clog << "\rDone.                 \n";
+}
