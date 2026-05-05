b6a504a64597691ad9026a40feeb3e9e88e438e9
HurayrahB <huray2007@gmail.com>
2026-05-04 23:58:59 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -136,7 +136,7 @@ void perlin_spheres() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 400;
-    cam.samples_per_pixel = 100;
+    cam.samples_per_pixel = 150;
     cam.max_depth         = 50;
 
     cam.vfov     = 20;
